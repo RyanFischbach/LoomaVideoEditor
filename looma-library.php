3eef89bd900ada66b4e659a37491b9cdbdad3631
Hekate1 <cpk2000x@gmail.com>
2016-06-16 14:37:51 -0700

    Back-end part done
    Opens files and reads them and then sends it to the video player.
    Actually display not finished

--- looma-library.php
+++ looma-library.php
@@ -48,6 +48,7 @@ Description:  displays and navigates content folders for Looma 2
 						   "' data-ft='" .  $ext . 
 						   "' data-zm='" .  160 .
 						   "' data-pg=1" .
+                           "'data-txt='" . ($ext == "txt" ? getJSON($file, $path, $ext) : "yolo") .
 						   ">";
 					   
 				//text and tooltip for BUTTON		   
@@ -64,6 +65,11 @@ Description:  displays and navigates content folders for Looma 2
 					   
 		};  //end makeButton()
 	
+        function getJSON($file, $path, $ext) {
+            $realpath = realpath($path) . '/';
+            return file_get_contents($realpath . $file, $realpath, null, 0);
+        }
+        
 		function isEpaath($fp) {
 		
 			//echo "<br>DEBUG: in isEpaath, FP is " . $fp . " Substr is " . mb_substr($fp, -7, 7);
@@ -162,6 +168,7 @@ Description:  displays and navigates content folders for Looma 2
 				case "video":
 				case "mp4":
 				case "mov":
+                case "txt":
 		
 				case "image":
 				case "jpg":
