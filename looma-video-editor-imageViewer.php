a992b4303b240dda71a085c833aabba58fcb69f6
Aaron Brown <ab341@comcast.net>
2016-06-23 12:45:46 -0700

    Fixed thumbnail bug

--- looma-video-editor-imageViewer.php
+++ looma-video-editor-imageViewer.php
@@ -3,11 +3,21 @@
 <?php
 
     $imagePath = '../content/pictures/';
+    $imageArr = array();
     
     if (is_dir($imagePath))
     {
-        //$imageNameArr = scandir($imagePath);
+        //$imageArr = scandir($imagePath);
         $imageArr = glob($imagePath . "*.{jpg,jpeg,gif,png}", GLOB_BRACE);
+    }
+
+    // Remove all thumbnails from array of image previews
+    for ($i = 0; $i < count($imageArr); $i++)
+    {
+        if (strpos($imageArr[$i], "_thumb"))
+        {
+            array_splice($imageArr, $i, 1);
+        }
     }
     
 
@@ -15,7 +25,6 @@
 
     for ($i = 0; $i < count($imageArr); $i++)
     {
-        /*echo '<input class="imageOption" id="' . $imageNameArr[$i] . '" type="image" src="' . $imagePath . $imageNameArr[$i] . '" width="100" height="100" />';*/
         echo '<input class="imageOption" id="' . $imageArr[$i] . '" type="image" src="' . $imageArr[$i] . '" width="150" height="90" />';
     }
 ?>
