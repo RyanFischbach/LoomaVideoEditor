4808f047177fd5311d0263246939c7f25f6f7843
Aaron Brown <ab341@comcast.net>
2016-07-27 13:43:18 -0700

    Fixed font size

--- includes/video-editor-file-viewer.php
+++ includes/video-editor-file-viewer.php
@@ -72,71 +72,71 @@ else if ($type == "video")
 }
 */
                        
-    $path = '../content/' . $folder . '/';
-        $fileArr = array();
+$path = '../content/' . $folder . '/';
+$fileArr = array();
                                 
                                 
-                        foreach (new DirectoryIterator($path) as $fileInfo)
-                        {
-                            $file =  $fileInfo->getFilename();
+foreach (new DirectoryIterator($path) as $fileInfo)
+{
+    $file =  $fileInfo->getFilename();
 
-                            //DEBUG  echo "   found " . $file . "<br>";
+    //DEBUG  echo "   found " . $file . "<br>";
 
-                            //skip ".", "..", and any ".filename" and any filename with '_thumb' in the name
-                            if (($file[0]  == ".") || strpos($file, "_thumb") || $file == "thumbnail.png") continue;  
+    //skip ".", "..", and any ".filename" and any filename with '_thumb' in the name
+    if (($file[0]  == ".") || strpos($file, "_thumb") || $file == "thumbnail.png") continue;  
 
-                            if ($fileInfo -> isFile())
-                            {
-                                $ext = $fileInfo -> getExtension();
-                                $file = $fileInfo -> getFilename();
-                                $base = trim($fileInfo -> getBasename($ext), ".");  //$base is filename w/o the file extension
+    if ($fileInfo -> isFile())
+    {
+        $ext = $fileInfo -> getExtension();
+        $file = $fileInfo -> getFilename();
+        $base = trim($fileInfo -> getBasename($ext), ".");  //$base is filename w/o the file extension
 
-                                //If the file is a .txt file (used to store edited videos) it gives it the correct display name
-                                if(substr($file, strlen($file) - 4) == ".txt")
-                                {
-                                    $dn = str_replace('_', ' ', substr($file, 0, strlen($file) - 8) . "_Edited");
-                                }
-                                else
-                                {
-                                    // look in the database to see if this file has a DISPLAYNAME
-                                    $query = array('fn' => $file);
+        //If the file is a .txt file (used to store edited videos) it gives it the correct display name
+        if(substr($file, strlen($file) - 4) == ".txt")
+        {
+            $dn = str_replace('_', ' ', substr($file, 0, strlen($file) - 8) . "_Edited");
+        }
+        else
+        {
+            // look in the database to see if this file has a DISPLAYNAME
+            $query = array('fn' => $file);
 
-                                    $projection = array('_id' => 0, 
-                                                    'dn' => 1, 
-                                        );		
-                                    $activity = $activities_collection -> findOne($query, $projection);
+            $projection = array('_id' => 0, 
+                'dn' => 1, 
+            );		
+            $activity = $activities_collection -> findOne($query, $projection);
 
-                                    $dn = ($activity && array_key_exists('dn', $activity)) ? $activity['dn'] : $base;
-                                }
+            $dn = ($activity && array_key_exists('dn', $activity)) ? $activity['dn'] : $base;
+        }
                                 
-                                switch (strtolower($ext))
-                                {
-                                    case "video":
-                                    case "mp4":
-                                    case "mov":
+        switch (strtolower($ext))
+        {
+            case "video":
+            case "mp4":
+            case "mov":
 
-                                    case "image":
-                                    case "jpg":
-                                    case "png":
-                                    case "gif":
+            case "image":
+            case "jpg":
+            case "png":
+            case "gif":
 
-                                    case "audio":
-                                    case "mp3":
+            case "audio":
+            case "mp3":
 
-                                    case "pdf":
+            case "pdf":
 
-                                        makeButton($file, $path, $ext, $base, $dn, $path . $base . "_thumb.jpg");
-                                        break;
+            makeButton($file, $path, $ext, $base, $dn, $path . $base . "_thumb.jpg");
+            break;
 
-                                    case "txt":
-                                        makeButton($file, $path, $ext, $base, $dn, $path . substr($base, 0, strlen($base) - 4) . "_thumb.jpg");
-                                        break;
+            case "txt":
+                makeButton($file, $path, $ext, $base, $dn, $path . substr($base, 0, strlen($base) - 4) . "_thumb.jpg");
+                break;
 
-                                    default:
-                                        // ignore unknown filetypes
-                                        // echo "DEBUG: " . $fileInfo -> getFilename() . "unkown filetype in looma-library.php";
-                                };  //end SWITCH
-                            }
-                        }
+            default:
+                // ignore unknown filetypes
+                // echo "DEBUG: " . $fileInfo -> getFilename() . "unkown filetype in looma-library.php";
+            };  //end SWITCH
+    }
+}
                         
-        ?>
+?>
